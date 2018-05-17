511a505c9e490b0d3dd00976d1c6670ad01ba23a
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-05-17 14:04:32 -0600

    Made API project with MongoDB driver already installed.

---
+++ Study_App_API/Study_App_API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Study_App_API.Controllers {
+
+    public class HomeController : Controller {
+
+        // ip/home/index
+        public ActionResult Index() {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
