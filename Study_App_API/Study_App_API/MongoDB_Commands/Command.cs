845b5f16fddf565bd1e94e0c6a2b543ef5f1f92c
priyaVora <prvora89@gmail.com>
2018-05-18 14:15:51 -0600

    Added two files in MongoDB_Command Folder.

---
+++ Study_App_API/Study_App_API/MongoDB_Commands/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Study_App_API.MongoDB_Commands
+{
+    public class Command
+    {
+
+    }
+}
---
+++ Study_App_API/Study_App_API/MongoDB_Commands/Project_Commands.cs
@@ -0,0 +1,81 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Linq;
+using System;
+
+namespace Study_App_API.MongoDB_Commands
+{
+
+
+    public class Project_Commands
+    {
+        IMongoDatabase mongo;
+        public Project_Commands(string ConnectionString)
+        {
+
+        }
+
+        public void  DeleteFile(string Id)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteNote(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateFile(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile(string File)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateUser(string User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateGoals(string Goal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkGoalAsComplete(string Goal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AuthenticateUser(string AuthUser) {
+            throw new NotImplementedException();
+        } 
+
+        public void ShareFile(String Id, String Dictionary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetUser(String Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetUpComingGoals(string Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFilePreviews(string Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCollections(string Collection)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
